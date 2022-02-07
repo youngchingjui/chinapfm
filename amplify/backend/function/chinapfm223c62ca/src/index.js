37d9fca2e3dbb6484e2c52e23517adad77ceb946
youngchingjui <young.chingjui@gmail.com>
2022-02-07 12:02:26 +0800

    printing parameters to see

--- amplify/backend/function/chinapfm223c62ca/src/index.js
+++ amplify/backend/function/chinapfm223c62ca/src/index.js
@@ -1,5 +1,7 @@
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   // TODO implement
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  console.log("CONTEXT: \n" + JSON.stringify(context, null, 2));
   const response = {
     statusCode: 200,
     //  Uncomment below to enable CORS requests
@@ -7,7 +9,7 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  },
-    body: JSON.stringify("Hello from Lambda!"),
+    body: JSON.stringify("Hello from Lambda!" + event),
   };
   return response;
 };
